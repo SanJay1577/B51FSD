39f6d8b798515b195dfd42d9a0e0f7ce74c05989
SanJay1577 <sanjayprashanth1997@gmail.com>
2023-09-08 09:57:34 +0530

    Four pillars of opps with examples and getters and setter methods

--- OOPS/script.js
+++ OOPS/script.js
@@ -108,12 +108,127 @@ function ManfuctureCar(name, seat, engine, variant) {
     `);
   };
 }
+//prototypes ....
+ManfuctureCar.prototype.getNameOfCar = function () {
+  console.log(this.name);
+};
 
 const passat = new ManfuctureCar("passat", 4, 2, "petrol");
 console.log(passat);
 passat.getCarDetails();
+passat.getNameOfCar();
 
 // creates a obj manufatcure {}
 // this = {}
 // this.name = name
 // {name : "passat"}
+
+// Class
+// 4 Pillars of oops
+//Encapsulation -> ability to define a data with methods
+//Inheritance
+// Abstraction
+// polymorphism
+// class in js
+
+class CarProduction {
+  // Encapsulation
+  constructor(name, color, fuelVariant, engineCapacity) {
+    this.name = name;
+    this.color = color;
+    this.fuelVariant = fuelVariant;
+    this.engineCapacity = engineCapacity;
+  }
+  // businnes logic or methods
+  getDetails() {
+    console.log(`
+     Name : ${this.name}
+     Color : ${this.color}
+     Fuel-Type : ${this.fuelVariant}
+     Engine-Capacity : ${this.engineCapacity} liter
+    `);
+  }
+
+  getspecialDetails() {
+    console.log(`Special Details are ${this.fuelVariant} engine`);
+  }
+}
+
+// Inheritance
+class Features extends CarProduction {
+  constructor(
+    name,
+    color,
+    fuelVariant,
+    engineCapacity,
+    safety,
+    model,
+    type,
+    seatType
+  ) {
+    super(name, color, fuelVariant, engineCapacity);
+    this._safety = safety;
+    this.model = model;
+    this.type = type;
+    this.seatType = seatType;
+  }
+  // getter method
+  get safety() {
+    return this._safety;
+  }
+
+  //setter method
+  set safety(newStarRating) {
+    if (newStarRating < 0) {
+      console.log("Error : star rating cannot be less than 0");
+    } else if (newStarRating > 5) {
+      console.log("Error : star rating cannot be greate than 5");
+    } else {
+      this._safety = newStarRating;
+    }
+  }
+
+  getCarFeatures() {
+    //abstraction..
+    let bhp = (this.engineCapacity * 100) / 1.8;
+    console.log(`
+      Star Ratings : ${this._safety}
+      Model : ${this.model}
+      Seats : ${this.seatType} seats
+      Car-Body Type : ${this.type}
+      BHP : ${bhp.toFixed(1)} BHP
+    `);
+  }
+  // polymorphism
+  getspecialDetails() {
+    console.log(`Special Details are ${this.seatType} seats`);
+  }
+}
+
+const car1 = new CarProduction("polo", "red", "diesel", 1.5);
+const car2 = new CarProduction("tiguan", "grey", "petrol", 4);
+console.log(car1);
+car1.getDetails();
+console.log(car2);
+car2.getDetails();
+
+const car3 = new Features(
+  "Jetta",
+  "lava blue",
+  "petrol",
+  4,
+  5,
+  "Topline",
+  "SUV",
+  "leather"
+);
+console.log(car3);
+car3.getDetails();
+car3.bhp = 400; // cannot do this
+car3.getCarFeatures();
+
+console.log(car3.safety);
+car3.safety = -1; // throw and error
+car3.safety = 4; // change to 4
+console.log(car3.safety);
+car3.getspecialDetails();
