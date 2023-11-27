ade7d807be9286b03301dfe8e4874c73624fc4b3
SanJay1577 <sanjayprashanth1997@gmail.com>
2023-11-27 09:54:40 +0530

    atlast admin activated

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 const connectionString = process.env.MONGO_URL;
 
-const localString = "mongodb://127.0.0.1:27017/Docapp";
+//const localString = "mongodb://127.0.0.1:27017/Docapp";
 async function dbConnection() {
   try {
-    const client = new MongoClient(localString);
+    const client = new MongoClient(connectionString);
     await client.connect();
     console.log("Database connected");
     return client;
